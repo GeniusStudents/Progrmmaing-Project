121a9b8ea820902861e1ff05476bdbb82782ca0b
AsadShahi <78696271+AsadShahi@users.noreply.github.com>
2022-11-13 09:56:14 +0430

    wrapper classes
    Wrapper class in JavaScript language

---
+++ WrapperClass Using JS/app.js
@@ -0,0 +1,20 @@
+//java script has three wrapper types 1: String 2: Boolean 3:Number
+
+
+
+//vonverting String to string
+// object class to primitive data type
+let language ='JavaScript'
+let temp=new String(language);
+console.log(temp);
+
+
+//converting number(Integer) to int 
+let   n= new Number(4)
+console.log(4);
+
+
+let num = new Number(10);
+let str= new String('JS');
+let bol = new Boolean(false);
+
